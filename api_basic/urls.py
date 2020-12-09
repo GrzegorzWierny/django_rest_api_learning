d87d59b388e401378b2b2b28a9fc6b337dfc05ac
Grzegorz Wierny <grzegorz.wierny@gmail.com>
2020-12-09 21:46:41 +0100

    GenericAPIView & Authentication added

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     # path('article/', article_list),
     # path('article_detail/<int:pk>/', article_detail),
     path('article/', ArticleAPIView.as_view()),
     path('article_detail/<int:pk>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
+    path('generic/article/', GenericAPIView.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from .models import Article
-from .serializers import ArticleSerializer
+from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin, mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request, id)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request):
+        self.destroy(request, id)
 
 
 class ArticleAPIView(APIView):
